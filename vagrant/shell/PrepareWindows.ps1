77697c638472d0d7abc281f5bd4519cd889c0fbc
Rob Reynolds <ferventcoder@gmail.com>
2016-01-10 18:06:33 -0600

    Prepare box shell script
    When weird things like IE ESC not being disabled come up, have the
    option of running a script to set everything appropriately.

---
+++ vagrant/shell/PrepareWindows.ps1
@@ -0,0 +1,11 @@
+# Adapted from http://stackoverflow.com/a/29571064/18475
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+Stop-Process -Name Explorer -Force
+Write-Output "IE Enhanced Security Configuration (ESC) has been disabled."
+
+# http://techrena.net/disable-ie-set-up-first-run-welcome-screen/
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
+Write-Output "IE first run welcome screen has been disabled."
