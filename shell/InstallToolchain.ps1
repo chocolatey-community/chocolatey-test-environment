4dfb61746f2ccbf46aa9b1bc13657e438b59943a
Bob Tanner <tanner@real-time.com>
2018-01-17 22:03:05 -0600

    Latest version of bitvise-ssh-server is broken in chocolatey

--- shell/InstallToolchain.ps1
+++ shell/InstallToolchain.ps1
@@ -1,4 +1,5 @@
 if (Get-Command "choco.exe" -ErrorAction SilentlyContinue) 
 { 
-    "7zip", "bitvise-ssh-server", "git", "notepadplusplus" | ForEach-Object -Process {choco install $_}
+#    "7zip", "bitvise-ssh-server", "git", "notepadplusplus" | ForEach-Object -Process {choco install $_}
+    "7zip", "git", "notepadplusplus" | ForEach-Object -Process {choco install $_}
 } 
