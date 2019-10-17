5967f086a273c7c933b0d69ef782a979eb9fe2f2
Jerome Bell <jeromebell@gmail.com>
2019-10-17 13:42:10 -0500

    Created Chocolatey package to set up a local test environment for Chocolatey packages;

---
+++ devops/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿vagrant plugin install sahara
+
+$currentDirectory = Split-Path $MyInvocation.MyCommand.Path
+$sourceDir = "chocolatey-test-environment"
+$repoUrl = "https://github.com/chocolatey-community/chocolatey-test-environment.git"
+
+# exit if directory already exists
+if ($(test-path $sourceDir -pathtype container) -eq $true)
+{
+	write-error "Cannot clone repo, folder '$sourceDir' already exists."
+	exit 1
+}
+
+# clone repo
+git clone $repoUrl
+ 
+cd $sourceDir
+ 
+# bring up test VM
+vagrant up
+
+# create base line snapshot
+vagrant sandbox on
+
+# test run of package install
+vagrant provision
